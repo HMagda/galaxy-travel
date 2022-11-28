f28fb0c3e79e357e0df71a79a705f0a414847f0b
Magdalena <magdalenahyra@wp.pl>
2022-11-28 23:47:26 +0100

    Edited placeholders styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import styles from './Footer.module.scss';
 import {FaFacebook, FaTwitter, FaYoutube, FaTiktok} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import {initialState} from '../../redux/store';
+import styles from './Footer.module.scss';
 import SignupForm from '../SignupForm/SignupForm';
 import SocialIcon from '../SocialIcon/SocialIcon';
-import {initialState} from '../../redux/store';
 
 function toUrlSlug(string) {
   return '/' + encodeURI(string.toLowerCase().trim().replaceAll(' ', '-'));
@@ -21,15 +21,12 @@ const Footer = () => {
           <SocialIcon url='https://facebook.com' name='Facebook'>
             <FaFacebook className={styles.social_icon} />
           </SocialIcon>
-
           <SocialIcon url='https://twitter.com' name='Twitter'>
             <FaTwitter className={styles.social_icon} />
           </SocialIcon>
-
           <SocialIcon url='https://youtube.com' name='Youtube'>
             <FaYoutube className={styles.social_icon} />
           </SocialIcon>
-
           <SocialIcon url='https://tiktok.com' name='TikTok'>
             <FaTiktok className={styles.social_icon} />
           </SocialIcon>
@@ -43,7 +40,6 @@ const Footer = () => {
 
       <div className={styles.links_wrapper}>
         <h1>Useful links</h1>
-
         <div className={styles.links_container}>
           {subPages.map((subPage) => (
             <Link to={toUrlSlug(subPage)} key={subPage} className={styles.link}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -38,29 +38,6 @@ export const initialState = {
       ],
     },
   },
-  users: [
-    {
-      name: {first: 'Jane', last: 'Doe'},
-      location: {country: 'USA'},
-      login: {uuid: 11111},
-      dob: {age: 18},
-      picture: {large: 'https://randomuser.me/api/portraits/men/34.jpg'},
-    },
-    {
-      name: {first: 'Jane', last: 'Doe'},
-      location: {country: 'USA'},
-      login: {uuid: 222222},
-      dob: {age: 18},
-      picture: {large: 'https://randomuser.me/api/portraits/men/34.jpg'},
-    },
-    {
-      name: {first: 'Jane', last: 'Doe'},
-      location: {country: 'USA'},
-      login: {uuid: 333333},
-      dob: {age: 18},
-      picture: {large: 'https://randomuser.me/api/portraits/men/34.jpg'},
-    },
-  ],
 };
 
 const store = createStore(
